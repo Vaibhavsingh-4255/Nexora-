689b2104e32eb7569aefad989de9e7119bf85e49
VaibhavSingh-4255 <minecraftnoir45@gmail.com>
2026-07-25 00:02:04 +0530

    replace mongo db with fire base firestore

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
-
-connectDB();
 
 app.use(cors());
 app.use(express.json());
